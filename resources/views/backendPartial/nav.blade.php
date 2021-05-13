c4fd0531f99b9069ec20cf243762002df5c713f7
martogasha <m.gachanga97@gmail.com>
2021-05-13 14:45:18 +0300

    selling from purchase

--- resources/views/backendPartial/nav.blade.php
+++ resources/views/backendPartial/nav.blade.php
@@ -69,7 +69,7 @@
                             <svg class="svg-icon" id="p-dash4" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <path d="M21.21 15.89A10 10 0 1 1 8 2.83"></path><path d="M22 12A10 10 0 0 0 12 2v10z"></path>
                             </svg>
-                            <span class="ml-4">Purchase</span>
+                            <span class="ml-4">Sell</span>
                             <svg class="svg-icon iq-arrow-right arrow-active" width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                                 <polyline points="10 15 15 20 20 15"></polyline><path d="M4 4h7a4 4 0 0 1 4 4v12"></path>
                             </svg>
@@ -77,7 +77,7 @@
                         <ul id="sale" class="iq-submenu collapse" data-parent="#iq-sidebar-toggle">
                             <li class="">
                                 <a href="{{url('purchase')}}">
-                                    <i class="las la-minus"></i><span>List Purchase</span>
+                                    <i class="las la-minus"></i><span>List Products for Sale</span>
                                 </a>
                             </li>
                         </ul>
