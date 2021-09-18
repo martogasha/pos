e08c8fde7f0e8c68547f5a6e3c6b057350fa0b41
Your Name <you@example.com>
2021-09-18 17:06:44 +0300

    fgsergerr

--- resources/views/backend/quotation.blade.php
+++ resources/views/backend/quotation.blade.php
@@ -2,81 +2,60 @@
     <div class="content-page">
         <div class="container-fluid">
             <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+            <div class="page-header text-blue-d2">
+                <h1 class="page-title text-secondary-d1">
+                    Invoice
+                    <small class="page-info">
+                        <i class="fa fa-angle-double-right text-80"></i>
+                        ID: #111-222
+                    </small>
+                </h1>
 
-            <div class="page-content container">
-                <div class="page-header text-blue-d2">
-                    <h1 class="page-title text-secondary-d1">
-                        Invoice
-                        <small class="page-info">
-                            <i class="fa fa-angle-double-right text-80"></i>
-                            ID: #111-222
-                        </small>
-                    </h1>
-
-                    <div class="page-tools">
-                        <div class="action-buttons">
-                            <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
-                                <i class="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
-                                Print
-                            </a>
-                            <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="PDF">
-                                <i class="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
-                                Export
-                            </a>
-                        </div>
+                <div class="page-tools">
+                    <div class="action-buttons">
+                        <a class="btn bg-white btn-light mx-1px text-95" href="#" data-title="Print">
+                            <i class="mr-1 fa fa-print text-primary-m1 text-120 w-2"></i>
+                            Print
+                        </a>
+                        <a class="btn bg-white btn-light mx-1px text-95" href="#" id="PDF">
+                            <i class="mr-1 fa fa-file-pdf-o text-danger-m1 text-120 w-2"></i>
+                            Export
+                        </a>
                     </div>
                 </div>
+            </div>
 
-                <div class="container px-0">
-                    <div class="row mt-4">
-                        <div class="col-12 col-lg-10 offset-lg-1">
-                            <div class="row">
-                                <div class="col-12">
-                                    <div class="text-center text-150">
-                                        <i class="fa fa-book fa-2x text-success-m2 mr-1"></i>
-                                        <span class="text-default-d3">Bootdey.com</span>
-                                    </div>
+            <div class="page-content container" id="receipt">
+                <div class="col-12 col-lg-10 offset-lg-1">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="text-center text-150">
+                                <div>
+                                    <img src="{{asset('assets/images/icons.png')}}" class="center" alt="Cinque Terre">
+                                    <br>
+                                    <h5 style="font-size: 18px"><b>IT Consultant, CCTV installation,Computer,Laptop Sales,Data Backup and Recovery</b></h5>
+                                    <h5 style="font-size: 15px;color: #2d3748"><span style="color: darkblue">A/c</span>: 01192559085500 (<b style="color: green">Co-op Bank</b>) / <span style="color: darkblue">Till</span>: 000798 (<b style="color: green">Davix Designs</b>)</h5>
+
+                                    <hr style="border: 2px solid red"/>
+
                                 </div>
+
                             </div>
-                            <!-- .row -->
-
-                            <hr class="row brc-default-l1 mx-n1 mb-4" />
-
+                        </div>
+                    </div>
                             <div class="row">
                                 <div class="col-sm-6">
                                     <div>
                                         <span class="text-sm text-grey-m2 align-middle">To:</span>
                                         <span class="text-600 text-110 text-blue align-middle">Alex Doe</span>
                                     </div>
-                                    <div class="text-grey-m2">
-                                        <div class="my-1">
-                                            Street, City
-                                        </div>
-                                        <div class="my-1">
-                                            State, Country
-                                        </div>
-                                        <div class="my-1"><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> <b class="text-600">111-111-111</b></div>
-                                    </div>
-                                </div>
-                                <!-- /.col -->
-
-                                <div class="text-95 col-sm-6 align-self-start d-sm-flex justify-content-end">
-                                    <hr class="d-sm-none" />
-                                    <div class="text-grey-m2">
-                                        <div class="mt-1 mb-2 text-secondary-m1 text-600 text-125">
-                                            Invoice
-                                        </div>
-
-                                        <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">ID:</span> #111-222</div>
-
-                                        <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Issue Date:</span> Oct 12, 2019</div>
-
-                                        <div class="my-2"><i class="fa fa-circle text-blue-m2 text-xs mr-1"></i> <span class="text-600 text-90">Status:</span> <span class="badge badge-warning badge-pill px-25">Unpaid</span></div>
-                                    </div>
                                 </div>
                                 <!-- /.col -->
                             </div>
-
+                <div class="container px-0">
+                    <div class="row mt-4">
+                        <div class="col-12 col-lg-10 offset-lg-1">
+                            <!-- .row -->
                             <div class="mt-4">
                                 <div class="row text-600 text-white bgc-default-tp1 py-25">
                                     <div class="d-none d-sm-block col-1">#</div>
@@ -94,7 +73,27 @@
                                         <div class="d-none d-sm-block col-2 text-95">$10</div>
                                         <div class="col-2 text-secondary-d2">$20</div>
                                     </div>
-
+                                    <div class="row mb-2 mb-sm-0 py-25">
+                                        <div class="d-none d-sm-block col-1">1</div>
+                                        <div class="col-9 col-sm-5">Domain registration</div>
+                                        <div class="d-none d-sm-block col-2">2</div>
+                                        <div class="d-none d-sm-block col-2 text-95">$10</div>
+                                        <div class="col-2 text-secondary-d2">$20</div>
+                                    </div>
+                                    <div class="row mb-2 mb-sm-0 py-25">
+                                        <div class="d-none d-sm-block col-1">1</div>
+                                        <div class="col-9 col-sm-5">Domain registration</div>
+                                        <div class="d-none d-sm-block col-2">2</div>
+                                        <div class="d-none d-sm-block col-2 text-95">$10</div>
+                                        <div class="col-2 text-secondary-d2">$20</div>
+                                    </div>
+                                    <div class="row mb-2 mb-sm-0 py-25">
+                                        <div class="d-none d-sm-block col-1">1</div>
+                                        <div class="col-9 col-sm-5">Domain registration</div>
+                                        <div class="d-none d-sm-block col-2">2</div>
+                                        <div class="d-none d-sm-block col-2 text-95">$10</div>
+                                        <div class="col-2 text-secondary-d2">$20</div>
+                                    </div>
                                     <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
                                         <div class="d-none d-sm-block col-1">2</div>
                                         <div class="col-9 col-sm-5">Web hosting</div>
@@ -183,13 +182,6 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>
-
-                                <hr />
-
-                                <div>
-                                    <span class="text-secondary-d1 text-105">Thank you for your business</span>
-                                    <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Pay Now</a>
                                 </div>
                             </div>
                         </div>
@@ -352,7 +344,26 @@
 
 <!-- app JavaScript -->
 <script src="assets/js/app.js"></script>
-</body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.9.2/html2pdf.bundle.js"></script>
 
+</body>
+<script>
+    window.onload = function () {
+        document.getElementById("PDF")
+            .addEventListener("click", () => {
+                const invoice = this.document.getElementById("receipt");
+                console.log(invoice);
+                console.log(window);
+                var opt = {
+                    margin: 1,
+                    filename: 'receipt.pdf',
+                    image: { type: 'jpeg', quality: 0.98 },
+                    html2canvas: { scale: 2 },
+                    jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+                };
+                html2pdf().from(invoice).set(opt).save();
+            })
+    }
+</script>
 <!-- Mirrored from iqonic.design/themes/posdash/html/backend/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Mar 2021 21:27:27 GMT -->
 </html>
